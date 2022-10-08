8f22f1a277ce2ec120c8230d41dd8be81559b221
Nithitha <109504541+Nithibalan@users.noreply.github.com>
2022-10-08 11:06:47 +0530

    Add files via upload

---
+++ design/design/Form2.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace design
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button33 = new System.Windows.Forms.Button();
+            this.button32 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button33
+            // 
+            this.button33.Location = new System.Drawing.Point(1346, 802);
+            this.button33.Name = "button33";
+            this.button33.Size = new System.Drawing.Size(75, 23);
+            this.button33.TabIndex = 45;
+            this.button33.Text = "NEXT";
+            this.button33.UseVisualStyleBackColor = true;
+            this.button33.Click += new System.EventHandler(this.button33_Click);
+            // 
+            // button32
+            // 
+            this.button32.Location = new System.Drawing.Point(1183, 802);
+            this.button32.Name = "button32";
+            this.button32.Size = new System.Drawing.Size(117, 23);
+            this.button32.TabIndex = 44;
+            this.button32.Text = "PREVIOUS";
+            this.button32.UseVisualStyleBackColor = true;
+            this.button32.Click += new System.EventHandler(this.button32_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(1285, 54);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(83, 17);
+            this.label7.TabIndex = 42;
+            this.label7.Text = "SATURDAY";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1104, 54);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 17);
+            this.label6.TabIndex = 41;
+            this.label6.Text = "FRIDAY";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(908, 54);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 17);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "THURSDAY";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(715, 54);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(96, 17);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "WEDNESDAY";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(529, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 17);
+            this.label3.TabIndex = 38;
+            this.label3.Text = "TUESDAY";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(331, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 17);
+            this.label2.TabIndex = 37;
+            this.label2.Text = "MONDAY";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(73, 74);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1478, 722);
+            this.flowLayoutPanel1.TabIndex = 36;
+            this.flowLayoutPanel1.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(128, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 43;
+            this.label1.Text = "SUNDAY";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(739, 9);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(72, 17);
+            this.label8.TabIndex = 46;
+            this.label8.Text = "JANUARY";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(817, 9);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 17);
+            this.label9.TabIndex = 47;
+            this.label9.Text = "label9";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1593, 837);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button33);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button32);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button33;
+        private System.Windows.Forms.Button button32;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
---
+++ design/design/Form2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace design
+{
+    public partial class Form2 : Form
+    {
+        int month;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        public static void months()
+        {
+
+            int[] ma = new int[35] { 0, 0, 0, 0, 0, 0, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 0, 0, 0, 0 };
+           
+            
+            
+        }
+
+        
+        
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            
+            flowLayoutPanel1.Controls.Clear();
+            month++;
+
+            for (int i = 1; i <= 35; i++)
+            {
+                Button button = new Button();
+                button.Height = 110;
+                button.Width = 140;
+                button.Text = i.ToString();
+                flowLayoutPanel1.Controls.Add(button);
+                string currentMonth = DateTime.Now.ToString("MM");
+                string currentYear = DateTime.Now.Year.ToString();
+                label8.Text = currentMonth;
+                label9.Text = currentYear;
+                months();
+
+          
+            }
+            
+           
+            
+           
+            
+            
+        
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+         
+            for (int i = 1; i <= 35; i++)
+            {
+                Button button = new Button();
+                button.Height = 110;
+                button.Width = 140;
+                button.Text = i.ToString();
+                flowLayoutPanel1.Controls.Add(button);
+                string currentMonth = DateTime.Now.Month.ToString("MM");
+                string currentYear = DateTime.Now.Year.ToString();
+                label8.Text = currentMonth;
+                label9.Text = currentYear;
+              
+            }
+            
+           
+            
+            
+            
+
+           
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.Controls.Clear();
+            month--;
+            
+            for (int i = 1; i <= 35; i++)
+            {
+                Button button = new Button();
+                button.Height = 110;
+                button.Width = 140;
+                button.Text = i.ToString();
+                flowLayoutPanel1.Controls.Add(button);
+                string currentMonth = DateTime.Now.Month.ToString("MM");
+                string currentYear = DateTime.Now.Year.ToString();
+                label8.Text = currentMonth;
+                label9.Text = currentYear;
+                
+                
+            }
+            
+            
+            
+           
+            
+           
+            
+        }
+
+        
+           
+
+
+    }
+}
